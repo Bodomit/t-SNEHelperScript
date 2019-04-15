bc542fc8a148acd6af321b0f877f08840fe91720
Glen Brown <gbrown29@qub.ac.uk>
2019-04-15 16:21:44 +0100

    Reading from matfile. Added n_components and perplexity args. Saving t-sne embeddings to output (both numpy and matfile. Plotting untested.

--- mat2tsne.py
+++ mat2tsne.py
@@ -16,7 +16,7 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
 
-def main(in_path, out_path, perplexity):
+def main(in_path, out_path, n_components, perplexity):
     print("Embedding Visualiser")
     in_path = os.path.abspath(os.path.expanduser(in_path))
 
@@ -27,31 +27,41 @@ def main(in_path, out_path, perplexity):
     print("Args:")
     print("\tInput path:         ", in_path)
     print("\tOutput filepath:    ", out_path)
+    print("\tN components:       ", n_components)
     print("\tPerplexity:         ", perplexity)
 
     # Create the output directory.
     os.makedirs(out_path)
 
-    # Load the embeddings from mat file.
-    embeddings = sio.loadmat(in_path)
+    # Load the embeddings from mat file. Get the first array in the matfile.
+    matfiledata = sio.loadmat(in_path)
+    for k in matfiledata:
+        if isinstance(matfiledata[k], np.ndarray):
+            embeddings = matfiledata[k]
 
-    # Run T-SNE to reduce down to 2-d.
-    embeddings = TSNE(n_components=2, random_state=42).fit_transform(embeddings)
+    # Run T-SNE to reduce down to n-components.
+    reduced_embeddings = TSNE(n_components=n_components, perplexity=perplexity, random_state=42, verbose=1).fit_transform(embeddings)
 
-    # Create the pandas dataset.
-    df = pd.DataFrame(data=embeddings, columns=["x", "y"])
-    df["x"] = df["x"].astype('float32')
-    df["y"] = df["y"].astype('float32')
+    # Save as numpy array and matfile.
+    print("Saving numpy array...")
+    np.save(os.path(out_path, "TSNE_N{}_p{}.npy".format(n_components, perplexity)), embeddings)
+    print("Saving matfile...")
+    sio.savemat(os.path(out_path, "TSNE_N{}_p{}.mat".format(n_components, perplexity)), mdict={"tsne": reduced_embeddings})
 
-    # Plot
-    chart = sns.lmplot(data=df, x='x', y='y')
+    # Create the pandas dataset and plot.
+    print("Plotting...")
+    df = pd.DataFrame(data=embeddings)
+    columns = df.columns.values.tolist()
+    chart = sns.lmplot(data=df, x=columns[0], y=columns[1])
     chart.savefig(os.path.join(out_path, "tsne_p{}.png".format(perplexity)))
+    print("Complete!")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Tries to visualise ')
-    parser.add_argument("input-path", help="Path to the checkpoint file or direcoty.")
+    parser.add_argument("input", help="Path to the checkpoint file or direcoty.")
     parser.add_argument("--output-path", "-o")
-    parser.add_argument("--perplexity", "-p", default=30)
+    parser.add_argument("--n-components", "-n", default=2)
+    parser.add_argument("--perplexity", "-p", default=30.0)
     args = parser.parse_args()
 
-    main(args.input_path, args.output_path, args.perplexity)
+    main(args.input, args.output_path, args.n_components, args.perplexity)
